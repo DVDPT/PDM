9a7c3f2a54d7a67b78370a89fbff623c7e2158d5
Sorcha <sorcha08@hotmail.com>
2012-05-11 15:33:10 +0100

    add activity User Info

--- code/src/pt/isel/adeetc/meic/pdm/UserInfoActivity.java
+++ code/src/pt/isel/adeetc/meic/pdm/UserInfoActivity.java
@@ -1,7 +1,11 @@
 package pt.isel.adeetc.meic.pdm;
 
 import android.app.Activity;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 
 /**
  * Created by IntelliJ IDEA.
@@ -11,8 +15,35 @@ import android.os.Bundle;
  * To change this template use File | Settings | File Templates.
  */
 public class UserInfoActivity extends Activity {
+
+    private TextView _userName;
+    private TextView _numberOfTweets;
+    private TextView _numberOfFollowers;
+    private TextView _numberOfFollowings;
+    private ImageView _userImage; 
+
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.user_info);
+
+        
+        _userName = (TextView) findViewById(R.id.user_info_name);
+        _numberOfTweets = (TextView) findViewById(R.id.user_info_tweets);
+        _numberOfFollowers = (TextView) findViewById(R.id.user_info_follwers);
+        _numberOfFollowings = (TextView) findViewById(R.id.user_info_follwers);
+        _userImage = (ImageView)findViewById(R.id.user_info_image);
     }
+    
+    
+    private void changeUserInfo(String name, int nTweets, int followers, int followings, Uri urlImage){
+        
+        _userName.setText(name);
+        _numberOfTweets.setText(Integer.toString(nTweets));
+        _numberOfFollowers.setText(Integer.toString(followers));
+        _numberOfFollowings.setText(Integer.toString(followings));
+        _userImage.setImageURI(urlImage);
+    }
+    
+    
 }
