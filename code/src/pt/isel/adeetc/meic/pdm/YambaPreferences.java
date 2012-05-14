ee7789049c5acf65fd631ad123f7c9b6b4787bca
DVD <amazing.red@gmail.com>
2012-05-14 12:14:29 +0100

    Added YambaPreferences.

---
+++ code/src/pt/isel/adeetc/meic/pdm/YambaPreferences.java
@@ -0,0 +1,30 @@
+package pt.isel.adeetc.meic.pdm;
+
+public final class YambaPreferences
+{
+    private YambaPreferences()
+    {
+    }
+
+    public static String userNamePropName = "userName";
+
+    public static String userPasswordPropName = "userPass";
+
+    public static String apiBaseUrl = "baseUrl";
+
+
+    public static String maxCharactersOnStatus = "maxCharacters";
+
+    public static String maxCharactersOnStatusTimeline = "maxCharsInTimeline";
+
+    public static String maxStatusShownOnTimeline = "maxTweets";
+
+
+    public static String timeLineFetchedAutomaticallyPropName = "timelineFetchedAutomaticallly";
+
+    public static String timelineRefreshPeriodPropName = "timelineRefreshPeriod";
+
+    public static String numberOfStatusPreservedPropName = "numberOfStatusPreserved";
+
+
+}
